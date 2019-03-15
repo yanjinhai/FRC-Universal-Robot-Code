dcdc6493b9f000926d3c81b9fb51a2f01b27e876
Jinhai Yan <jinhaiyan777@gmail.com>
2019-03-14 18:08:33 -0700

    Commit 1

---
+++ src/main/java/frc/robot/Customizer.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import java.util.*;
+
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * Currently features:
+ * 
+ * -Custom drive motor ports
+ */
+public class Customizer {
+
+    static {
+        System.out.println("****Customizing...****");
+        customizeDriveMotorPorts();
+    }
+
+    private static void customizeDriveMotorPorts(){
+        Scanner consoleScanner = new Scanner(System.in);
+        System.out.println("How many drive speedcontrollers do you have?");
+        String userInput = consoleScanner.nextLine();
+        int numberOfDriveMotors;
+        while(true){
+            try {
+                numberOfDriveMotors = Integer.parseInt(userInput);
+                break;
+            } catch (Exception e) {
+                System.out.println("Error: Please enter an integer expression. Try again.");
+            }
+        }
+        SpeedController[] driveMotors = new SpeedController[numberOfDriveMotors];
+        for(int i = 0; i < numberOfDriveMotors; i++){
+            System.out.println("What is the PWM Channel/CAN ID.");
+            switch(){
+
+            }
+            driveMotors[i] = new;
+        }
+        consoleScanner.close();
+    }
+}
